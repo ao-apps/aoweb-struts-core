7aefbf5536cc77df6a57466f7dc1e6cb94dd6eb5
AO Industries, Inc <support@aoindustries.com>
2018-11-29 06:22:01 -0600

    New version of aoserv-client.

--- src/main/java/com/aoindustries/website/clientarea/control/monitor/MySQLReplicationMonitorAction.java
+++ src/main/java/com/aoindustries/website/clientarea/control/monitor/MySQLReplicationMonitorAction.java
@@ -1,6 +1,6 @@
 /*
  * aoweb-struts-core - Core API for legacy Struts-based site framework with AOServ Platform control panels.
- * Copyright (C) 2000-2013, 2016, 2017  AO Industries, Inc.
+ * Copyright (C) 2000-2013, 2016, 2017, 2018  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
